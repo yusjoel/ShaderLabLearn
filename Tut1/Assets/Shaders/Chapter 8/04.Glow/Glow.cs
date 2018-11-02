6a79f1660f988d9f29c4aebfd05ed2ed3d83179e
yusjoel <joelyusj@hotmail.com>
2018-11-02 16:42:36 +0800

    完成Glow

---
+++ Tut1/Assets/Shaders/Chapter 8/04.Glow/Glow.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class Glow : MonoBehaviour {
+
+    public Material GaussianBlur;
+
+    public Material Material;
+
+    public float BlurAmount = 2;
+
+    float[] weightsH, weightsV;
+    Vector4[] offsetsH, offsetsV;
+
+    private void Start()
+    {
+        // Calculate weights/offsets for horizontal pass
+        calcSettings(1.0f / Screen.width, 0, out weightsH, out offsetsH);
+        // Calculate weights/offsets for vertical pass
+        calcSettings(0, 1.0f / Screen.height, out weightsV, out offsetsV);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (GaussianBlur != null && Material != null)
+        {
+            int w = source.width;
+            int h = source.height;
+            var buffer = RenderTexture.GetTemporary(w, h, 0);
+            var blurredTexture = RenderTexture.GetTemporary(w, h, 0);
+
+            GaussianBlur.SetVectorArray("Offsets", offsetsH);
+            GaussianBlur.SetFloatArray("Weights", weightsH);
+            Graphics.Blit(source, buffer, GaussianBlur);
+
+            GaussianBlur.SetVectorArray("Offsets", offsetsV);
+            GaussianBlur.SetFloatArray("Weights", weightsV);
+            Graphics.Blit(buffer, blurredTexture, GaussianBlur);
+
+            Material.SetTexture("_BlurredTexture", blurredTexture);
+            Graphics.Blit(source, destination, Material);
+
+            RenderTexture.ReleaseTemporary(buffer);
+            RenderTexture.ReleaseTemporary(blurredTexture);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+
+    float gaussianFn(float x)
+    {
+        return (1.0f / Mathf.Sqrt(2 * Mathf.PI * BlurAmount * BlurAmount)) * Mathf.Exp(-(x * x) / (2 * BlurAmount * BlurAmount));
+    }
+
+    void calcSettings(float w, float h, out float[] weights, out Vector4[] offsets)
+    {
+        // 15 Samples
+        weights = new float[15];
+        offsets = new Vector4[15];
+        // Calculate values for center pixel
+        weights[0] = gaussianFn(0);
+        offsets[0] = new Vector4(0, 0);
+        float total = weights[0];
+        // Calculate samples in pairs
+        for (int i = 0; i < 7; i++)
+        {
+            // Weight each pair of samples according to Gaussian function
+            float weight = gaussianFn(i + 1);
+            weights[i * 2 + 1] = weight;
+            weights[i * 2 + 2] = weight;
+            total += weight * 2;
+            // Samples are offset by 1.5 pixels, to make use of
+            // filtering halfway between pixels
+            float offset = i * 2 + 1.5f;
+            Vector2 offsetVec = new Vector2(w, h) * offset;
+            offsets[i * 2 + 1] = offsetVec;
+            offsets[i * 2 + 2] = -offsetVec;
+        }
+        // Divide all weights by total so they will add up to 1
+        for (int i = 0; i < weights.Length; i++)
+            weights[i] /= total;
+    }
+}
---
+++ Tut1/Assets/Shaders/Chapter 8/04.Glow/Glow.shader
@@ -0,0 +1,50 @@
+﻿Shader "3D Graphics with XNA Games Studio 4.0/Chapter 8/04.Glow"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+
+	SubShader
+	{
+		Pass
+		{
+			ZTest Always
+			Cull Off
+			ZWrite Off
+
+			CGPROGRAM
+			#pragma vertex VertexShaderFunction
+			#pragma fragment PixelShaderFunction
+
+			#include "UnityCG.cginc"
+
+			sampler2D _MainTex;
+			sampler2D _BlurredTexture;
+
+			struct VertexShaderOutput
+			{
+				float4 Position : SV_POSITION;
+				half2 UV : TEXCOORD0;
+			};
+
+			VertexShaderOutput VertexShaderFunction(appdata_img input)
+			{
+				VertexShaderOutput output;
+				output.Position = UnityObjectToClipPos(input.vertex);
+				output.UV = input.texcoord;
+				return output;
+			}
+
+			float4 PixelShaderFunction(VertexShaderOutput input) : SV_Target
+			{
+				float4 unblurred = tex2D(_MainTex, input.UV);
+				float4 blurred = tex2D(_BlurredTexture, input.UV);
+				float4 additive = saturate(unblurred + blurred);
+
+				return additive;
+			}
+			ENDCG
+		}
+	}
+}
